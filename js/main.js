2b7f372c83f2ef96c210a53e22aec49fbb4ef5fc
Truong <nguyenvantruong9876@gmail.com>
2022-07-22 09:54:29 +0700

    du an moi

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+//oop nhan viên
+function Nhanvien(_maNV, _tenNV, _dateNV, _emailNV, _chucvuNV, _tongluongNV, _xeploaiNV) {
+    this.manv = _maNV;
+    this.tennv = _tenNV;
+    this.datenv = _dateNV;
+    this.emailnv = _emailNV;
+    this.chucvunv = _chucvuNV;
+    this.tongluongnv = _tongluongNV;
+    this.xeploainv = _xeploaiNV;
+
+    function themnhanvien() {}
+}
